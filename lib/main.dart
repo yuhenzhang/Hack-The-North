0620126cfd184dd0701fc0c0537755aad698cf6d
yuhenzhang <yuhengzhang1016@gmail.com>
2021-01-17 02:45:36 -0800

    firebase video upload working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:html';
+
+import 'package:firebase/firebase.dart' as fb;
 
 void main() {
   runApp(MyApp());
@@ -6,11 +9,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hybrid Meter',
+      title: 'Meter One',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Meter One Demo'),
     );
   }
 }
@@ -47,18 +51,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  fb.UploadTask _uploadTask;
 
   @override
   Widget build(BuildContext context) {
@@ -69,10 +62,24 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color(0xFFE4EAFF),
       appBar: AppBar(
+        leading: new Padding(
+          padding: const EdgeInsets.all(1.0),
+          child: Image.asset(
+            "crop.jpg",
+          ),
+        ),
+        backgroundColor: Colors.white,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          "Meter One Demo",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -94,21 +101,93 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            FloatingActionButton.extended(
+              heroTag: 'picker',
+              elevation: 5,
+              backgroundColor: Color(0XFF69A3FF),
+              hoverElevation: 0,
+              label: Row(
+                children: <Widget>[
+                  Icon(Icons.file_upload),
+                  SizedBox(width: 10),
+                  Text('Upload Video')
+                ],
+              ),
+              onPressed: () => uploadImage(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            StreamBuilder<fb.UploadTaskSnapshot>(
+              stream: _uploadTask?.onStateChanged,
+              builder: (context, snapshot) {
+                final event = snapshot?.data;
+
+                // Default as 0
+                double progressPercent = event != null
+                    ? event.bytesTransferred / event.totalBytes * 100
+                    : 0;
+
+                if (progressPercent == 100) {
+                  return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Video Uploaded'));
+                } else if (progressPercent == 0) {
+                  return SizedBox();
+                } else {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LinearProgressIndicator(
+                      value: progressPercent,
+                    ),
+                  );
+                }
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  /// A "select file/folder" window will appear. User will have to choose a file.
+  /// This file will be then read, and uploaded to firebase storage;
+  uploadImage() async {
+    // HTML input element
+    InputElement uploadInput = FileUploadInputElement();
+    uploadInput.click();
+
+    uploadInput.onChange.listen(
+      (changeEvent) {
+        final file = uploadInput.files.first;
+        final reader = FileReader();
+        // The FileReader object lets web applications asynchronously read the
+        // contents of files (or raw data buffers) stored on the user's computer,
+        // using File or Blob objects to specify the file or data to read.
+        // Source: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+
+        reader.readAsDataUrl(file);
+        // The readAsDataURL method is used to read the contents of the specified Blob or File.
+        //  When the read operation is finished, the readyState becomes DONE, and the loadend is
+        // triggered. At that time, the result attribute contains the data as a data: URL representing
+        // the file's data as a base64 encoded string.
+        // Source: https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
+
+        reader.onLoadEnd.listen(
+          // After file finiesh reading and loading, it will be uploaded to firebase storage
+          (loadEndEvent) async {
+            uploadToFirebase(file);
+          },
+        );
+      },
+    );
+  }
+
+  uploadToFirebase(File imageFile) async {
+    final filePath = '${DateTime.now()}.mp4';
+    setState(() {
+      _uploadTask = fb
+          .storage()
+          .refFromURL('gs://hack-the-north-adc4b.appspot.com')
+          .child(filePath)
+          .put(imageFile);
+    });
   }
 }
